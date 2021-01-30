b031b3f9613ce7df8de836235b39555886ba3cd4
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2021-01-30 21:46:27 +0000

    Login logic

---
+++ GeekBrainsTestProject/LoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  GeekBrainsTestProject
+//
+//  Created by Denis Mordvinov on 24.01.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    private var testLogin = "Test"
+    private var testPassword = "Test"
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func onLoginPressed(_ sender: Any) {
+        if loginTextField.text == testLogin && passwordTextField.text == testPassword {
+            return
+        }else {
+            
+            let alert = UIAlertController(title: "Error", message: "Wrong login or password", preferredStyle: .alert)
+            let alertAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
+            alert.addAction(alertAction)
+            present(alert, animated: true, completion: nil)
+        }
+    }
+
+
+}
+
