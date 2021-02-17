d74684a8452626d2cda9f8a1cabc33a7211feed1
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2021-02-17 20:39:21 +0000

    Animations on LoginView

--- GeekBrainsTestProject/LoginViewController.swift
+++ GeekBrainsTestProject/LoginViewController.swift
@@ -14,8 +14,11 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        animateTextFields()
+        animateSubmitButton()
     }
 
     @IBAction func onLoginPressed(_ sender: Any) {
@@ -28,5 +31,28 @@ class LoginViewController: UIViewController {
             alert.addAction(alertAction)
             present(alert, animated: true, completion: nil)
         }
+    }
+
+    func animateTextFields() {
+        let offset = view.bounds.width
+        loginTextField.transform = CGAffineTransform(translationX: -offset, y: 0)
+        passwordTextField.transform =  CGAffineTransform(translationX: offset, y: 0)
+
+        UIView.animate(withDuration: 1, delay: 1, options: .curveEaseInOut, animations: {
+            self.loginTextField.transform = .identity
+            self.passwordTextField.transform = .identity
+        }, completion: nil)
+    }
+
+    func animateSubmitButton() {
+        let animation = CASpringAnimation(keyPath: "transform.scale")
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.stiffness = 200
+        animation.mass =   2
+        animation.duration = 2
+        animation.beginTime = CACurrentMediaTime() + 1
+        animation.fillMode = .backwards
+        self.loginButton.layer.add(animation, forKey: nil)
     }
 }
