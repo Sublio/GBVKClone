28d328212495df43f34581d8a314d5b69165c329
Denis Mordvinov <denis.mordvinov@corp.badoo.com>
2021-02-21 01:02:14 +0000

    Alphabetical index for Friends List

--- GeekBrainsTestProject/FriendsTableViewController.swift
+++ GeekBrainsTestProject/FriendsTableViewController.swift
@@ -85,6 +85,15 @@ class FriendsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         cell.backgroundColor = .clear
     }
+    
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        let nonFilteredFriends = notFilteredFriends.joined()
+        var firstNames = [String]()
+        for friend in nonFilteredFriends {
+            firstNames.append(String(friend.name.characterAtIndex(index: 0)!))
+        }
+        return firstNames.uniqueElementsFrom(array: firstNames)
+    }
 
     func setGradientToTableView() {
         let hexColors: [CGColor] = [
