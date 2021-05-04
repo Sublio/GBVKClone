602336e9029e41d56fcd7f7103267720bc381fa1
Denis Mordvinov <sublio1260@gmail.com>
2021-05-05 00:40:37 +0100

    Two cells for NewsFeed

---
+++ GeekBrainsTestProject/Cells/PhotoPostTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PhotoPostTableViewCell.swift
+//  GeekBrainsTestProject
+//
+//  Created by Denis Mordvinov on 05.05.2021.
+//
+
+import UIKit
+
+class PhotoPostTableViewCell: UITableViewCell {
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
