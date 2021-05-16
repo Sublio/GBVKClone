8f4bea82d2faf96019a67a02d43230ae87998e04
Denis Mordvinov <sublio1260@gmail.com>
2021-05-16 07:07:08 +0100

    update iOS target version for build script

---
+++ GeekBrainsTestProject/Services/KeychainService.swift
@@ -0,0 +1,21 @@
+//
+//  KeyChainsManager.swift
+//  GeekBrainsTestProject
+//
+//  Created by Denis Mordvinov on 16.05.2021.
+//
+
+import Foundation
+import Security
+
+// Arguments for the keychain queries
+let kSecClassValue = NSString(format: kSecClass)
+let kSecAttrAccountValue = NSString(format: kSecAttrAccount)
+let kSecValueDataValue = NSString(format: kSecValueData)
+let kSecClassGenericPasswordValue = NSString(format: kSecClassGenericPassword)
+let kSecAttrServiceValue = NSString(format: kSecAttrService)
+let kSecMatchLimitValue = NSString(format: kSecMatchLimit)
+let kSecReturnDataValue = NSString(format: kSecReturnData)
+let kSecMatchLimitOneValue = NSString(format: kSecMatchLimitOne)
+
+class 
--- build_test.sh
+++ build_test.sh
@@ -1,1 +1,1 @@
-xcodebuild -project GeekBrainsTestProject.xcodeproj -scheme GeekBrainsTestProject -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.4' build test | xcpretty
+xcodebuild -project GeekBrainsTestProject.xcodeproj -scheme GeekBrainsTestProject -destination 'platform=iOS Simulator,name=iPhone 12,OS=14.5' build test | xcpretty
