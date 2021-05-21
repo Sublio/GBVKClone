2aa168dd3a1d3ed4f320a0d18f968260eb4797c2
Denis Mordvinov <sublio1260@gmail.com>
2021-05-21 20:04:04 +0100

    new newsFeed layout p1

---
+++ GeekBrainsTestProject/Cells/TextPostTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TextPostTableViewCell.swift
+//  GeekBrainsTestProject
+//
+//  Created by Denis Mordvinov on 21.05.2021.
+//
+
+import UIKit
+
+class TextPostTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var textPost: UITextView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- GeekBrainsTestProject/Controllers/NewsFeedTableViewController.swift
+++ GeekBrainsTestProject/Controllers/NewsFeedTableViewController.swift
@@ -17,7 +17,7 @@ class NewsFeedTableViewController: UITableViewController {
         tableView.estimatedRowHeight = 600
         let gradientView = GradientView()
         self.tableView.backgroundView = gradientView
-        vkService.getNewsFeedTextPosts()
+        //vkService.getNewsFeedTextPosts()
         //vkService.getNewsFeedPhotoPosts()
         
     }
@@ -29,7 +29,7 @@ class NewsFeedTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return 3
     }
 
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
@@ -37,18 +37,24 @@ class NewsFeedTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "textPostCell", for: indexPath) as! TextNewsFeedTableViewCell
-        cell.postAuthor.text = "David"
-        cell.views.text = "22"
-        cell.comments.text = "109"
-        cell.likes.text = "20"
-        cell.postAvatarImage.image = UIImage(named: "face1")
-        cell.reposts.text = "20"
-
-        return cell
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "headerCell") as! NewsHeaderTableViewCell
+            return cell
+        }else if indexPath.row == 1{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "textPostCell") as! TextPostTableViewCell
+            cell.textPost.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
+            return cell
+        }else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "likesCell") as! LikesTableViewCell
+            return cell
+        }
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
     }
 }
