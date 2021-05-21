945dc352cac177fb974dcfcfcc27c92b56cb6563
Denis Mordvinov <sublio1260@gmail.com>
2021-05-21 20:43:34 +0100

    almost there

--- GeekBrainsTestProject/Controllers/NewsFeedTableViewController.swift
+++ GeekBrainsTestProject/Controllers/NewsFeedTableViewController.swift
@@ -42,7 +42,6 @@ class NewsFeedTableViewController: UITableViewController {
             return cell
         }else if indexPath.row == 1{
             let cell = tableView.dequeueReusableCell(withIdentifier: "textPostCell") as! TextPostTableViewCell
-            cell.textPost.text = "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda."
             return cell
         }else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "likesCell") as! LikesTableViewCell
@@ -55,6 +54,10 @@ class NewsFeedTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableView.automaticDimension
+        if indexPath.row == 1 {
+            return 180
+        }else {
+            return 40
+        }
     }
 }
